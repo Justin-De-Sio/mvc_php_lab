b21bdf1e81b690d1c7df8c3255f3e4e92cfd5504
Justin-De-Sio <desio.j@live.fr>
2021-12-20 15:06:55 +0100

    exception

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,29 +1,40 @@
 <?php
+require('controller/frontend.php');
 
-require('model/frontend.php');
-
-function listPosts()
-{
-    $posts = getPosts();
-
-    require('view/frontend/listPostsView.php');
-}
-
-function post()
-{
-    $post = getPost($_GET['id']);
-    $comments = getComments($_GET['id']);
-
-    require('view/frontend/postView.php');
-}
-
-function addComment($postId, $author, $comment)
-{
-    $affectedLines = postComment($postId, $author, $comment);
-
-    if ($affectedLines === false) {
-        die('Impossible d\'ajouter le commentaire !');
-    } else {
-        header('Location: index.php?action=post&id=' . $postId);
+try { // On essaie de faire des choses
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts') {
+            listPosts();
+        }
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
+            }
+            else {
+                // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                }
+                else {
+                    // Autre exception
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            }
+            else {
+                // Autre exception
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
     }
+    else {
+        listPosts();
+    }
+}
+catch(Exception $e) { // S'il y a eu une erreur, alors...
+    echo 'Erreur : ' . $e->getMessage();
 }

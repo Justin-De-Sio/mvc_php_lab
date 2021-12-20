1797efde50fa9b2506ffd86ba02202bde66de16c
Justin-De-Sio <desio.j@live.fr>
2021-12-20 15:33:46 +0100

    gérer les erreurs

--- model/frontend.php
+++ model/frontend.php
@@ -48,9 +48,8 @@ function dbConnect()
     $pass = '';
     $dbname = 'test';
     $host = 'localhost';
-    try {
-        return new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $user, $pass);
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
+
+    return new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $user, $pass);
+
+
 }
---
+++ view/errorView.php
@@ -0,0 +1,4 @@
+
+<h1>Erreur</h1>
+<h4>voici le message d'erreur :</h4>
+<p><?= $errorMessage ?></p>

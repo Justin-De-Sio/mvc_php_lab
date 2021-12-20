397635163039ccc8cb8e735e17b3773118d6e706
Justin-De-Sio <desio.j@live.fr>
2021-12-20 14:39:21 +0100

    ajout de la fonctionnalité de commentaire

---
+++ model/frontend.php
@@ -0,0 +1,56 @@
+<?php
+
+
+function getPosts()
+{
+    $db = dbConnect();
+    return $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr 
+                                FROM posts 
+                                ORDER BY creation_date DESC LIMIT 0, 5');
+}
+
+function getPost($postId)
+{
+    $db = dbConnect();
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') 
+                                AS creation_date_fr
+                                FROM posts
+                                WHERE id = ?');
+    $req->execute(array($postId));
+    return $req->fetch();
+}
+
+function getComments($postId)
+{
+    $db = dbConnect();
+    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\')
+                                    AS comment_date_fr 
+                                    FROM comments 
+                                    WHERE post_id = ? 
+                                    ORDER BY comment_date DESC');
+    $comments->execute(array($postId));
+
+    return $comments;
+}
+
+function postComment($postId, $author, $comment)
+{
+    $db = dbConnect();
+    $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+    $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+    return $affectedLines;
+}
+
+function dbConnect()
+{
+    $user = 'root';
+    $pass = '';
+    $dbname = 'test';
+    $host = 'localhost';
+    try {
+        return new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $user, $pass);
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+}

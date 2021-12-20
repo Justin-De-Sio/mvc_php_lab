a00c3d62cc6d3c4717242c9441df346be7bcb030
Justin-De-Sio <desio.j@live.fr>
2021-12-20 10:46:55 +0100

    afficher un commentaire

--- indexView.php
+++ indexView.php
@@ -12,26 +12,24 @@
 
 
 <?php
-if (isset($posts)) {
-    while ($data = $posts->fetch())
-    {
-        ?>
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($data['title']) ?>
-                <em>le <?= $data['creation_date_fr'] ?></em>
-            </h3>
+while ($data = $posts->fetch())
+{
+    ?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($data['title']) ?>
+            <em>le <?= $data['creation_date_fr'] ?></em>
+        </h3>
 
-            <p>
-                <?= nl2br(htmlspecialchars($data['content'])) ?>
-                <br />
-                <em><a href="#">Commentaires</a></em>
-            </p>
-        </div>
-        <?php
-    }
-    $posts->closeCursor();
+        <p>
+            <?= nl2br(htmlspecialchars($data['content'])) ?>
+            <br />
+            <em><a href="post.php?id=<?= $data['id'] ?>">Commentaires</a></em>
+        </p>
+    </div>
+    <?php
 }
+$posts->closeCursor();
 ?>
 </body>
 </html>
